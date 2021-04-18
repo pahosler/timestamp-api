01703ec0dd4e38ca921dad7225ea7b5580a17968
pahosler <pahosler@gmail.com>
2021-04-18 03:48:53 -0500

    now sends date

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send(Math.floor(Date.now() / 1000);
 });
 
 app.listen(PORT, function () {
