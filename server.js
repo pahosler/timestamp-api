a0118870771839f32e7b3ccfca48f989d291c5ea
pahosler <pahosler@gmail.com>
2021-04-18 04:07:46 -0500

    I am dumb

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000;
+const date =()=>Math.floor(Date.now() / 1000);
 
-app.get('/', function (req, res) {
-  res.send(Math.floor(Date.now() / 1000));
-});
+app.get('/', 
+  function (req, res, next) {
+    res.send(`${date()}`);
+  }
+);
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
